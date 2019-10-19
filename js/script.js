e1ee6083e7be783f3f85ded42014945a8e4d8e16
Hubert Pol <hubertpol@wp.pl>
2019-10-19 22:01:35 +0200

    correct errors in the task handler event

--- js/script.js
+++ js/script.js
@@ -9,81 +9,22 @@ const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!'); 
-    
-    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    
-    /* [Done] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
-    
-    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
-    
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
-    //console.log(articleSelector);
-    
-    /* find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
-
-    /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
-    //console.log('targetArticle', targetArticle);
 }
-
 const links = document.querySelectorAll('.titles a');
-
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
 
-{
-    const optArticleSelector = '.post',
-          optTitleSelector = '.post-title',
-          optTitleListSelector = '.titles';
-    function generateTitleLinks(){
-
-    /* remove contents of titleList */
-    const titleList = document.querySelector(optTitleListSelector);//.innerHTML = '';
-    console.log('titleList', titleList);
-    titleList.innerHTML = '';
-    
-    /* for each article */
-    
-    //const articles = document.querySelectorAll('.post');
-    let html = '';
-    for(let article of articles){        
-    }
-    /* get the article id */
-    
-    const articleId = article.getAttribute('id');
-    
-    /* find the title element */
-   
-    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    console.log('articleTitle', articleId);
-    
-    /* get the title from the title element */
-    
-    /* create HTML of the link */
-    
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-    console.log('linkHTML', linkHTML);
-    
-    /* insert link into titleList */
-    
-    html = html + linkHTML;
-    console.log('html', html);
-}
-    titleList.innerHTML = html;
-}
-
-generateTitleLinks();
