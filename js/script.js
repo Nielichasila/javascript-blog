092b354ad0dde108cc7d38b644235107379b1677
Hubert Pol <hubertpol@wp.pl>
2019-10-19 22:08:39 +0200

    correct style JS

--- js/script.js
+++ js/script.js
@@ -28,3 +28,24 @@ for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
 
+{
+const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+function generateTitleLinks(){
+    const titleList = document.querySelector(optTitleListSelector);
+    console.log('titleList', titleList);
+    titleList.innerHTML = '';   
+    const articles =  document.querySelectorAll('.post');
+    let html = '';
+    for(let article of articles){        
+        const articleId = article.getAttribute('id'); 
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;   
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        html = html + linkHTML;
+    }
+        console.log('html', html);
+        titleList.innerHTML = html;
+}
+generateTitleLinks();
+}
